cd9373c729bce3ceedc7d14bc402bfcb3a4a77b0
abdoohossamm <abdoohossamm@outlook.com>
2022-12-03 23:46:12 +0200

    Add a main section to the index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import HeadTag from '../components/head/HeadTag'
 import NavBar from "../components/navigation/NavBar";
+import Main from "../components/home/Main";
 export default function Home() {
   return (
     <main className="bg-white px-10">
         <HeadTag />
         <NavBar />
+        <Main/>
 
     </main>
   )
