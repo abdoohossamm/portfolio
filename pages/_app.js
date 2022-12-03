98d0f3e8ff9991045f19f4bacb20e904a90a1521
abdoohossamm <abdoohossamm@outlook.com>
2022-12-03 23:26:39 +0200

    Add navbar for the future components

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import "../styles/nav.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import HeadTag from '../components/head/HeadTag'
+import NavBar from "../components/navigation/NavBar";
 export default function Home() {
   return (
     <main className="bg-white px-10">
         <HeadTag />
+        <NavBar />
 
     </main>
   )
