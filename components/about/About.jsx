c3b3edd352af9b778b4fcda78df2074942927553
abdoohossamm <abdoohossamm@outlook.com>
2022-12-04 23:28:39 +0200

    restyle the about section

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -5,31 +5,28 @@ import CardItem from "./CardItem";
 import {cards} from "../data";
 
 function About() {
-    const cardStyle = "bg-gradient-to-b from-cyan-300 to-teal-400 " +
-        "rounded-2xl border-2 border-transparent p-6 " +
-        "text-center hover:from-cyan-700 hover:to-teal-500 hover:text-white"
-    const iconStyle = "mx-auto text-2xl mb-2"
     return (
-        <section id="about" className="min-h-screen">
-            <div className={"text-center p-10"}>
+        <section id="about" className="min-h-screen pb-12">
+            <div className={"text-center p-6"}>
                 <h5 className="text-2xl pt-2">Get To Know</h5>
-                <h2 className="p-10 text-5xl py-2 text-teal-600 font-medium">About Me</h2>
+                <h2 className="p-10 text-4xl lg:text-5xl py-2 text-teal-600 font-medium">About Me</h2>
             </div>
-            <div id="about-container" className={"grid p-10 sm:grid-cols-1 lg:grid-cols-3 gap-10"}>
+            <div id="about-container" className={"grid px-10 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-10"}>
 
                 <div id="about-me" className="grid grid-cols-1 aspect-square rounded-3xl bg-gradient-to-b from-teal-500 sm:w-3/5 lg:w-5/6 sm:mx-auto"
                 >
                     <Image src={Profile} alt={"Profile Picture"}
-                           className="rounded-3xl overflow-hidden transform rotate-6 transition duration-200 delay-50 hover:rotate-0"
+                           className="rounded-3xl overflow-hidden transform rotate-6 transition duration-200 delay-50 hover:rotate-0 "
                     />
                 </div>
 
-                <div id="about-content" className={"pt-6 lg:col-span-2"}>
+                <div id="about-content" className={"py-6 lg:col-span-2"}>
                     <div id="about-cards" className={"grid gap-3 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3"}>
                         {cards.map((card, i) => (
                             <CardItem value={card} key={i} />
                         ))}
                     </div>
+                    <h4 className={"text-xl px-6 py-4 text-gray-800 dark:text-sky-100"}>I am currently pursuing Bachelor Degree(Final Year) in Computer Science from University Of the People. I have 2+ years of experience in Web Development in different projects with different frameworks.</h4>
                 </div>
             </div>
         </section>
