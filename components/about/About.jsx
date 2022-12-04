a2914e46f71ce2b41f9ca43bbc58daec9e019ecf
abdoohossamm <abdoohossamm@outlook.com>
2022-12-05 01:12:39 +0200

    finish the dark mode in About section

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -9,12 +9,12 @@ function About() {
     return (
         <section id="about" className="min-h-screen pb-12">
             <div className={"text-center p-6"}>
-                <h5 className="text-2xl pt-2">Get To Know</h5>
+                <h5 className="text-2xl pt-2 dark:text-white">Get To Know</h5>
                 <h2 className="p-10 text-4xl lg:text-5xl py-2 text-teal-600 font-medium">About Me</h2>
             </div>
             <div id="about-container" className={"grid px-10 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-10"}>
 
-                <div id="about-me" className="grid grid-cols-1 aspect-square rounded-3xl bg-gradient-to-b from-teal-500 sm:w-3/5 lg:w-5/6 sm:mx-auto"
+                <div id="about-me" className="grid grid-cols-1 aspect-square rounded-3xl bg-gradient-to-b from-teal-500 dark:from-cyan-700 sm:w-3/5 lg:w-5/6 sm:mx-auto"
                 >
                     <Image src={Profile} alt={"Profile Picture"}
                            className="rounded-3xl overflow-hidden transform rotate-6 transition duration-200 delay-50 hover:rotate-0 "
@@ -27,7 +27,9 @@ function About() {
                             <CardItem value={card} key={i} />
                         ))}
                     </div>
-                    <h4 className={"text-xl px-6 py-4 text-gray-800 dark:text-sky-100"}>I am currently pursuing Bachelor Degree(Final Year) in Computer Science from University Of the People. I have 2+ years of experience in Web Development in different projects with different frameworks.</h4>
+                    <h4 className={"text-xl px-6 py-4 text-gray-800 dark:text-white"}>
+                        I am currently pursuing Bachelor Degree(Final Year) in Computer Science from University Of the People. I have 2+ years of experience in Web Development in different projects with different frameworks.
+                    </h4>
                     <Resume />
                 </div>
             </div>
