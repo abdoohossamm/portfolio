089330f8312e02f7784cba7506e2a5b168e56106
abdoohossamm <abdoohossamm@outlook.com>
2022-12-04 18:43:11 +0200

    Apply dark mode to Main section

--- components/home/Main.jsx
+++ components/home/Main.jsx
@@ -7,14 +7,14 @@ const Main = () => {
     return (
         <section id="main" className="min-h-screen">
             <div className={"text-center p-10"}>
-                <h3 className={"text-2xl pt-2"}>Hello, I'm</h3>
-                <h2 className={"text-4xl py-2 text-teal-600 font-medium"}>Abdalrahman Mohamed</h2>
-                <h3 className={"text-2xl pt-2"}>Fullstack developer</h3>
-                <h3 className={"text-2xl pb-2"}>(Backend-heavy)</h3>
-                <p className={"text-md pt-5 leading-8 text-gray-800"}>
+                <h3 className={"text-2xl pt-2 dark:text-white"}>Hello, I'm</h3>
+                <h2 className={"text-4xl lg:text-5xl py-2 text-teal-600 font-medium"}>Abdalrahman Mohamed</h2>
+                <h3 className={"text-2xl pt-2 dark:text-white"}>Fullstack developer</h3>
+                <h3 className={"text-2xl pb-2 dark:text-white"}>(Backend-heavy)</h3>
+                <p className={"text-md pt-5 leading-8 text-gray-800 dark:text-white"}>
                     Freelancer providing services for web developing, programming, and IT needs.
                 </p>
-                <p className={"text-md pb-5 leading-8 text-gray-800"}>
+                <p className={"text-md pb-5 leading-8 text-gray-800 dark:text-white"}>
                     Join me down below and let's get cracking!
                 </p>
 
@@ -31,7 +31,7 @@ const Main = () => {
                     Resume
                 </a>
             </div>
-            <div className={"relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden"}>
+            <div className={"relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-72 h-72 mt-10 overflow-hidden md:w-80 md:h-80 lg:w-96 lg:h-96"}>
                 <Image
                     src={waving}
                     alt={"waving avatar"}
