56921c9fd46f28449e7b55b1316ac8978d26789f
abdoohossamm <abdoohossamm@outlook.com>
2022-12-05 20:44:27 +0200

    Add project data type

--- components/types.ts
+++ components/types.ts
@@ -17,5 +17,14 @@ export interface Card {
     description: string;
 
 }
+export interface IProject {
+    name: string;
+    description: string;
+    image_path: string;
+    deployed_url: string;
+    github_url: string;
+    category: Category[];
+    key_techs: string[];
+}
 
 export type Category = "react" | "node" | "express" | "django" | "mongo" | "postgres" | "docker";
