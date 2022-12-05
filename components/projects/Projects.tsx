3c5db8f59a826610ccae2ec063ce5bdd019fcad4
abdoohossamm <abdoohossamm@outlook.com>
2022-12-06 00:10:23 +0200

    add Projects list to the Projects section

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProjectsList from "./projectList/ProjectsList";
 
 function Projects() {
     return (
@@ -7,7 +8,9 @@ function Projects() {
                 <h5 className="text-2xl pt-2 dark:text-white">What I Do</h5>
                 <h2 className="p-10 text-4xl lg:text-5xl py-2 text-teal-600 font-medium">My Projects</h2>
             </div>
-
+            <div className={"bg-gradient-to-l from-white to-teal-200 rounded-2xl px-6 py-2 dark:from-cyan-700 dark:to-cyan-900 dark:text-white"}>
+                <ProjectsList/>
+            </div>
         </section>
     );
 }
