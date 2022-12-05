cb54ed3c093b028b4811622d887c074b67cbe347
abdoohossamm <abdoohossamm@outlook.com>
2022-12-05 20:45:35 +0200

    Create Projects component for projects section

---
+++ components/projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Projects() {
+    return (
+        <section id="projects" className="min-h-screen pb-12">
+            <div className={"text-center p-6"}>
+                <h5 className="text-2xl pt-2 dark:text-white">What I Do</h5>
+                <h2 className="p-10 text-4xl lg:text-5xl py-2 text-teal-600 font-medium">My Projects</h2>
+            </div>
+
+        </section>
+    );
+}
+
+export default Projects;
