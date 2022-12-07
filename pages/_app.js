85a12f783cfe900a81b0936a49a05868243e3e24
abdoohossamm <abdoohossamm@outlook.com>
2022-12-07 19:39:24 +0200

    Install vercel analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import "../styles/about.css"
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
+  );
 }
 
 export default MyApp
