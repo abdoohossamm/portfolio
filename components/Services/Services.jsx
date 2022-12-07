2c9188d1890c2b49a363ed9aa327a83dab1a96d6
abdoohossamm <abdoohossamm@outlook.com>
2022-12-07 02:55:58 +0200

    Initial the services section.

---
+++ components/Services/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Services() {
+    return (
+        <section id="services" className="min-h-screen pb-12">
+            <div className={"text-center p-6"}>
+                <h5 className="text-2xl pt-2 dark:text-white">What I Offer</h5>
+                <h2 className="p-10 text-4xl lg:text-5xl py-2 text-teal-600 font-medium">Services</h2>
+            </div>
+
+
+        </section>
+    );
+}
+
+export default Services;
