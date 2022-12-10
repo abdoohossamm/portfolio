654966f0f71a80e762f1e0bd152065f72e3865f7
abdoohossamm <abdoohossamm@outlook.com>
2022-12-10 02:17:08 +0200

    apply react-awesome-slider to the Project card in projects section

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import "../styles/about.css"
+import "../styles/slider.css"
 import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
