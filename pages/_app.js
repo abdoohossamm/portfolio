ba3b8ecf2f106d68d3260ec8ebba3a4380d381bb
abdoohossamm <abdoohossamm@outlook.com>
2022-12-17 01:01:59 +0200

    Add css file for styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import "../styles/about.css"
 import "../styles/slider.css"
+import "../styles/contact.css"
 import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
