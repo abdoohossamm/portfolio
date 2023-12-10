1a5656675ae5fb0411a8d6a56c7725119a5f5846
abdoohossamm <abdoohossamm@outlook.com>
2023-12-10 23:08:00 +0200

    Update the skills and services.

--- components/data.ts
+++ components/data.ts
@@ -20,6 +20,7 @@ import {ImGit} from "react-icons/im";
 import {DiGoogleAnalytics, DiLinux} from "react-icons/di";
 import {GiTestTubes} from "react-icons/gi";
 import { FaGolang, FaJava } from "react-icons/fa6";
+import { SiScylladb, SiRabbitmq } from "react-icons/si";
 
 export const languages: Skill[] = [
     {
@@ -140,6 +141,16 @@ export const tools: Skill[] = [
         name: "MongoDB",
         level: "60",
     },
+    {
+        Icon: SiScylladb,
+        name: "Scylladb",
+        level: "70",
+    },
+    {
+        Icon: SiRabbitmq,
+        name: "Rabbitmq",
+        level: "60",
+    },
 ];
 
 export const otherSkills: Skill[] = [
@@ -195,22 +206,28 @@ export const cards: Card[] = [
 ];
 export const services: Service[] = [
     {
-        Icon: FaServer,
-        title: "Backend  Development",
+        Icon: MdOutlineDesignServices,
+        title: "Software engineering",
         about:
-            "Design and handle SQL and NoSQL databases, servers, APIs using <b>Django</b>, <b>Laravel</b>, and <b>Express</b> frameworks",
+            "With more than 3 years in tech companies as software engineer, and more than 1 year managing teams in 2 companies, I also has experience <b>designing software</b>, <b>choose the proper tools to build that software</b>, <b>choose and design its database</b>, <b>design the software architecture</b> to make it easy to maintain, scalable.",
     },
     {
         Icon: MdOutlineDesignServices,
-        title: "Frontend Development",
+        title: "DevOps Engineering",
         about:
-            "I can build a beautiful and scalable SPA using <b> HTML</b>,<b>CSS</b>,<b>JS</b> and can use any of the following frameworks <b>React.js</b>, <b>Next.js</b>, and <b>Vue.js</b> ",
+            "I have experience designing a DevOps architecture for companies, creating CI/CD pipeline with <b>GitHub actions</b>, <b>Jenkins</b>, I also have experience working with the following cloud providers <b>DigitalOcean</b>, <b>AWS</b>, <b>Azure</b>, also I can build an <b>Infrastructure as code</b> using many tools.",
+    },
+    {
+        Icon: FaServer,
+        title: "Backend Development",
+        about:
+            "Design and handle SQL and NoSQL databases, servers, APIs using <b>Django</b>, <b>Laravel</b>, and <b>Express</b> frameworks",
     },
     {
         Icon: AiOutlineApi,
         title: "API Development",
         about:
-            "I can develop robust REST API using <b>django-rest-framework</b> & <b>Node API</b>",
+            "I can develop robust REST API using <b>django</b>, <b>FastAPI</b>, <b>Golang(fiberAPI, Gin)</b>, <b>NodeJs(ExpressJS)</b>",
     },
     {
         Icon: RiComputerLine,
